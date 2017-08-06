35089fffdad405a738183cff9b3874dbb66167c3
alessandrocolleoni <colleoni.al@gmail.com>
2017-08-06 10:32:00 +0200

    Page layout
    Defined the page layout

---
+++ src/layouts/PageLayout/PageLayout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { IndexLink, Link } from 'react-router'
+import PropTypes from 'prop-types'
+import './PageLayout.scss'
+
+export const PageLayout = ({ children }) => (
+  <div className='container text-center'>
+    <IndexLink to='/reviews' activeClassName='page-layout__nav-item--active'>Reviews</IndexLink>
+    {' · '}
+    <Link to='/admin' activeClassName='page-layout__nav-item--active'>Admin</Link>
+    <div className='page-layout__viewport'>
+      {children}
+    </div>
+  </div>
+)
+PageLayout.propTypes = {
+  children: PropTypes.node,
+}
+
+export default PageLayout
